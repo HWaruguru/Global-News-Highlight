dd8c0cdf82c208d744660cf769e62cfc1a1b10d1
Waruguru <hannah.waruguru8@gmail.com>
2021-09-12 19:08:02 +0300

    Add the process function

--- app/request.py
+++ app/request.py
@@ -46,3 +46,25 @@ def get_articles_from_source(source_id):
     return articles_data
 
 
+def process_sources(sources_data):
+    '''
+    Function that converts source dict into source model
+    '''
+    sources = []
+    for source_data in sources_data:
+        source = Source(source_data['id'], source_data['name'], 
+         source_data['category'], source_data['description'], source_data['language'])
+        sources.append(source)
+    return sources
+
+def process_articles(articles_data):
+    '''
+    Function that converts articles dict into articles model
+    '''
+    articles = []
+    for article_data in articles_data:
+        article = Article(article_data['url'], article_data['title'], article_data['description'],
+                         article_data['urlToImage'], article_data['publishedAt'])
+        articles.append(article)
+    return articles
+
