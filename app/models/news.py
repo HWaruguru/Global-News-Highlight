c6d18b35ec1f8555d8b681c70ca353390147d665
Waruguru <hannah.waruguru8@gmail.com>
2021-09-12 15:58:18 +0300

    Add the articles and the sources model class

---
+++ app/models/news.py
@@ -0,0 +1,24 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+    def __init__(self, name, category, description, language):
+        self.name = name
+        self.category = category
+        self.description = description
+        self.language  = language
+
+
+class Article:
+    '''
+    Article class to define Article objects
+    '''
+    def __init__(self, url, title, description, publishedAt):
+        self.url = url
+        self.title = title
+        self.description = description
+        self.publishedAt = publishedAt
+
+
+
